0a2d1392b9136ed6c5f3252b0a4099b6facf2126
LaibaSh01 <laibasheikh12121@gmail.com>
2025-06-11 14:06:56 +0500

    development

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import Arrow from "./assets/svgs/Arrow";
 import Header from "./components/Header";
 import Marquee from "react-fast-marquee";
+import feedData from "./data/feedData.json";
 function App() {
   const marqueeContent = [
     { text: "SERTN AVS DEPLOYED TO EIGENLAYER TESTINET", link: "#" },
@@ -78,6 +80,11 @@ function App() {
       value: "$102.5",
     },
   ];
+
+  const groupedItems = feedData.filter((item) => item.group);
+  const normalItems = feedData.filter((item) => !item.group && !item.footer);
+  const footerItem = feedData.find((item) => item.footer);
+
   return (
     <div className="sertn-ai">
       <main>
@@ -148,7 +155,7 @@ function App() {
                 <div className="m-right"></div>
               </div>
               <div className="manifesto-table">
-                {manifesto.map((item, index) => (
+                {manifesto.map((item) => (
                   <div key={item.id} className="manifesto-item">
                     <div className="left-column">
                       <span className="item-number">{item.id}</span>
@@ -199,6 +206,152 @@ function App() {
                 <div></div>
                 <div></div>
               </div>
+              <div className="features-main-container">
+                <div className="accordion">
+                  <div className="accordion-item">
+                    <h3>Agentic Native Protocols</h3>
+                    <p>
+                      Agents require a different set of tools at their disposal.
+                      They can't rely on trust nor determine counterparty risk
+                      without all the facts. Instead, Agents will use protocols
+                      to ensure interoperability, model authenticity, and
+                      computational integrity.
+                    </p>
+                  </div>
+                  <div className="accordion-item">
+                    <h3 className="inactive">DATA-BACKED EXPERIENCES</h3>
+                    <p className="hide">
+                      Provide peace of mind in our user experiences with
+                      verifiable data-backed function calls. Imagine using
+                      unverified smart contracts, with expectations and
+                      guarantees as an afterthought. No need to trust a black
+                      box when you see the math yourself.
+                    </p>
+                  </div>
+                  <div className="accordion-item">
+                    <h3 className="inactive">
+                      Agentic Native PrINTEROPERABLE INTELLIGENCEotocols
+                    </h3>
+                    <p className="hide">
+                      With a system integrated into existing AI protocols, the
+                      power of AI verification is in your hands. Build cross
+                      chain applications to execute AI workflows with guarantees
+                      of atomic operations.
+                    </p>
+                  </div>
+                  <div className="accordion-item">
+                    <h3 className="inactive">ETHICAL ADVANCEMENTS</h3>
+                    <p className="hide">
+                      What's out of sight is actually top of mind. Responsible
+                      AI faces a myriad of challenges, from securing patient and
+                      client data to ensuring explainability in AI inference.
+                      Empower those affected by AI predictions to challenge or
+                      alter future outcomes using technologies such as
+                      Zero-Knowledge Machine Learning (zkML) and Fully
+                      Homomorphic Encryption (FHE).
+                    </p>
+                  </div>
+                  <div className="accordion-item">
+                    <h3 className="inactive">INFERENCE COMMERCE PROTOCOLS</h3>
+                    <p className="hide">
+                      Sertn makes it possible for projects, data scientists, and
+                      developers to easily deploy next-gen proprietary models
+                      without compromises or customer trust assumptions.
+                    </p>
+                  </div>
+                  <div className="accordion-item">
+                    <h3 className="gray">
+                      BUILD WITH US <Arrow />
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="marquee-section">
+          <div className="box">
+            <div className="marquee-tag">
+              <Marquee gradient={false} speed={40}>
+                <div className="logo-container">
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                </div>
+                <div className="logo-container">
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                  <div className="logo"></div>
+                </div>
+              </Marquee>
+            </div>
+          </div>
+        </section>
+
+        <section className="feed">
+          <div className="box">
+            <div className="feed-content">
+              <div className="feed-head">
+                <h2>Feed</h2>
+              </div>
+
+              {normalItems.map((item, index) => (
+                <div className="feed-main-content-item" key={index}>
+                  <div className="tab">
+                    <h4>{item.type}</h4>
+                  </div>
+                  <div className="info">
+                    <h6>{item.date}</h6>
+                    <h3>{item.title}</h3>
+                    <h6>
+                      {item.type} <Arrow />
+                    </h6>
+                  </div>
+                </div>
+              ))}
+
+              <div className="feed-main-content-item">
+                <div className="tab image"></div>
+                <div className="wrapper">
+                  {groupedItems.map((item, index) => (
+                    <div className="wrapper-content" key={index}>
+                      <h6>{item.date}</h6>
+                      <h3>{item.title}</h3>
+                      <h6>
+                        {item.type} <Arrow />
+                      </h6>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {footerItem && (
+                <div className="feed-main-content-item bottom">
+                  <div className="tab">
+                    <h4>SERTN - WE PROVIDE CERTAINTY FOR ON-CHAIN AI</h4>
+                  </div>
+                  <div className="info">
+                    <h3>{footerItem.title}</h3>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </section>
